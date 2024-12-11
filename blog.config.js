e73854485b60e9528551802661003bd2c6706182
ifixitup <36727369+ifixitup@users.noreply.github.com>
2024-12-11 16:03:36 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  title: '垃圾佬的博客',
+  author: '垃圾佬',
+  email: 'i@freetech.dynv6.net',
+  link: 'www.163.com',
+  description: '这里是垃圾佬日常折腾的记录。',
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
